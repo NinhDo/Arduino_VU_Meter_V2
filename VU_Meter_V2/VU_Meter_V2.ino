7c70f05a010730830a8bc96571653c56c64b020c
Ninh Do <doquininh@gmail.com>
2017-11-07 18:31:56 +0100

    Basic modes added. Interrupt handler added as well

--- VU_Meter_V2/VU_Meter_V2.ino
+++ VU_Meter_V2/VU_Meter_V2.ino
@@ -1,7 +1,14 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LEDPIN 6
+#define LEDPIN 6  // The datapin for the LED rings
 
+// Modes. Add or remove if need. Mostly for readability.
+#define SPIRAL 0
+#define EQUALIZER 1
+#define BREATHE 2
+#define NUMMODES 3
+
+// General LED ring info
 const uint8_t NUMPIXELS = 60;    // Total  number of pixels
 const uint8_t OUTER_START = 0;   // When the outer ring starts
 const uint8_t OUTER_END = 24;    // When the outer ring ends + 1 (for looping)
@@ -14,44 +21,44 @@ const uint8_t INNER_END = 60;    // When the inner ring ends 0 1 (for looping)
 // Neopixels are really bright, so you should keep these low unless you want a rave party
 // Green Light
 const uint8_t GREEN_R = 0;
-const uint8_t GREEN_G = 50;
+const uint8_t GREEN_G = 255;
 const uint8_t GREEN_B = 0;
 
 // Yellow Light
-const uint8_t YELLOW_R = 50;
-const uint8_t YELLOW_G = 25;
+const uint8_t YELLOW_R = 255;
+const uint8_t YELLOW_G = 200;
 const uint8_t YELLOW_B = 0;
 
 // Orange Light
-const uint8_t ORANGE_R = 50;
-const uint8_t ORANGE_G = 8;
+const uint8_t ORANGE_R = 255;
+const uint8_t ORANGE_G = 100;
 const uint8_t ORANGE_B = 0;
 
 // Red Light
-const uint8_t RED_R = 50;
+const uint8_t RED_R = 255;
 const uint8_t RED_G = 0;
 const uint8_t RED_B = 0;
 
-// Threshold of the colors (percentage). Red threshold is anything above ORANGE_THRESHOLD
-const uint8_t GREEN_THRESHOLD = 60;
-const uint8_t YELLOW_THRESHOLD = 75;
-const uint8_t ORANGE_THRESHOLD = 90;
-
 // The LED Rings, connected in series
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN);
 
-const uint8_t AUDIO_SIZE = 6;
-const uint8_t AUDIO_PINS[AUDIO_SIZE] = { A0, A1, A2, A3, A4, A5 }; 
-int16_t audioVals[AUDIO_SIZE] = { 0, 0, 0, 0, 0, 0 };
-uint8_t bands[6][10];
-const uint8_t SAMPLE_TIMES = 5;
+const uint8_t AUDIO_SIZE = 6;                                       // The number of filters we have. Change if needed
+const uint8_t AUDIO_PINS[AUDIO_SIZE] = { A0, A1, A2, A3, A4, A5 };  // The pins. Add or remove if needed.
+int16_t audioVals[AUDIO_SIZE];             
+
+uint8_t bands[6][10];                                               // 6 bands, each with 10 LEDs (I have 60 total). Change it to match your setup
+
+const uint8_t SAMPLE_TIMES = 5;                                     // To amplify the audio signal by reading multiple times. Change if needed
+
+uint8_t mode = EQUALIZER;                                           // The display mode.
 
 void setup() {
-  analogReference(INTERNAL);
+  analogReference(INTERNAL);                                        // The voltage from the audio is only a couple of millivolts, so INTERNAL is needed
   for (uint8_t i = 0; i < AUDIO_SIZE; i++) {
-    analogRead(AUDIO_PINS[i]);
+    analogRead(AUDIO_PINS[i]);                                      // Read analog pin to clear data
+    audioVals[i] = 0;                                               // Initialize the audioVals array
   }
-  //Serial.begin(9600);
+  Serial.begin(9600);
   for (uint8_t n = 0; n < AUDIO_SIZE; n++) {
     for (uint8_t i = 0; i < 2; i++) {
       bands[n][i] = INNER_START + i + n * 2;
@@ -73,9 +80,73 @@ void setup() {
   }
   
   pixels.begin();
+  attachInterrupt(0, switchMode, RISING);
 }
 
 void loop() {
+  switch(mode) {
+    case SPIRAL:
+      spiral();
+      break;
+    case EQUALIZER:
+      equalizer();
+      break;
+    default:
+      breathe();
+  }
+}
+
+void spiral() {
+  for(int8_t n = 0; n < 2; n++) {
+    for(int8_t i = 59; i > 0; i-=2) {
+      pixels.setPixelColor(i, 100 * n, 100 * n, 100 * n);
+      pixels.show();
+      delay(10);
+    }
+    for(uint8_t i = 0; i < NUMPIXELS; i+=2) {
+      pixels.setPixelColor(i, 100 * n, 100 * n, 100 * n);
+      pixels.show();
+      delay(10);
+    } 
+  }
+}
+
+void breathe() {
+  uint8_t b_red = 255;
+  uint8_t b_green = 0;
+  uint8_t b_blue = 0;
+  for(uint8_t x = 0; x < 6; x++) {
+    for(uint8_t n = 0; n < 255; n++) {
+      for(uint8_t i = 0; i < NUMPIXELS; i++) {
+        pixels.setPixelColor(i, b_red, b_green, b_blue);
+      }
+      pixels.show();
+
+      switch(x) {
+        case 0:
+        b_green++;
+        break;
+        case 1:
+        b_red--;
+        break;
+        case 2:
+        b_blue++;
+        break;
+        case 3:
+        b_green--;
+        break;
+        case 4:
+        b_red++;
+        break;
+        case 5:
+        b_blue--;
+        break;
+      }
+    }
+  }  
+}
+
+void equalizer() {
   // Zero out audioVals
   for (uint8_t i = 0; i < AUDIO_SIZE; i++) {
     audioVals[i] = 0;
@@ -83,25 +154,21 @@ void loop() {
   // Sample multiple times to increase effect. Audio signal is really weak from headphone jack
   for (uint8_t x = 0; x < SAMPLE_TIMES; x++) {
     for (uint8_t i = 0; i < AUDIO_SIZE; i++) {
-      audioVals[i] += analogRead(AUDIO_PINS[i]) * 2;
+      audioVals[i] += analogRead(AUDIO_PINS[i]);
     }
   }
-  /*for (uint8_t i = 0; i < AUDIO_SIZE; i++) {
-    audioVals[i] = constrain(map(audioVals[i], 0, 200, 0, 1023), 0, 1000);
-  }*/
-  
   
   for (uint8_t i = 0; i < AUDIO_SIZE; i++) {
     Serial.print(audioVals[i]);
     Serial.print("\t");
-    setPixel(i, audioVals[i]);
+    equalizerSetPixel(i, audioVals[i]);
   }
   Serial.println();
   pixels.show();
   delayMicroseconds(50);
 }
 
-void setPixel(uint8_t band, uint16_t value) {
+void equalizerSetPixel(uint8_t band, uint16_t value) {
   uint8_t red, green, blue;
   for (uint8_t i = 0; i <= 10; i++) {
     if(i < 2) {
@@ -125,5 +192,9 @@ void setPixel(uint8_t band, uint16_t value) {
       pixels.setPixelColor(bands[band][i], 0, 0, 0);
     }
   }
+}
+
+void switchMode() {
+  mode = (mode + 1) % 3;
 }
 
